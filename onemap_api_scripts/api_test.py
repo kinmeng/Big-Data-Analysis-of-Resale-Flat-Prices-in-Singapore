39ffbde177f35a2f35c6ad03222e88060f2b15e6
Tan Kin Meng <kinmeng.tan@gmail.com>
2022-09-18 20:23:28 +0800

    updated with lat/lon data
    unique_add_latlon.csv contains all unique lat lon for each unique address in our dataset
    
    combined_data.csv adds lat/lon and distance to central area as a column

--- onemap_api_scripts/api_test.py
+++ onemap_api_scripts/api_test.py
@@ -10,17 +10,14 @@ df = df[['unique_add']].drop_duplicates()
 
 def latlon(sample_df_row):
   base_url = "https://developers.onemap.sg/"
-  path= "/commonapi/search?searchVal={}&returnGeom={}&getAddrDetails={}".format(str(sample_df_row[0]),'Y','Y')
+  path= "/commonapi/search?searchVal={}&returnGeom={}&getAddrDetails={}&pageNum={}".format(str(sample_df_row),'Y','Y',1)
   result= requests.get(base_url+path)
-  #   print(str(sample_df_row[0]))
-  #   print(result.json())
+  print(str(sample_df_row))
   if result.json()['found'] >= 1:
     x, y=  result.json()['results'][0]['LONGITUDE'],result.json()['results'][0]['LATITUDE'] 
   else:
-    print(result.json())
     x, y = 'NULL','NULL'
   return x,y
-
 
 df.loc[:,'Lon'], df.loc[:,'Lat']= zip(*df['unique_add'].apply(latlon))
 df.to_csv('unique_add_latlon.csv')
